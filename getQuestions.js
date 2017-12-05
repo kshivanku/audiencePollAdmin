e7ba4b19b302cee6906a149fd3739188dc4c8011
kshivanku <kshivanku@gmail.com>
2017-12-05 02:54:39 -0500

    adding questions

--- getQuestions.js
+++ getQuestions.js
@@ -1,27 +1,56 @@
 var questions = [
     {
         "question_id": "HIM01",
-        "question_text": "Writer knows some information from Joanna's Yelp, Facebook, LinkedIn and Instagram; Which one should he bring up to seem less creepy?",
+        "question_text": "Writer knows some information from Yuan's Yelp, Uber, Seamless and Google Maps; Which one should he bring up to seem less creepy?",
         "option1": "Yelp",
-        "option2": "Facebook",
-        "option3": "LinkedIn",
-        "option4": "Instagram",
+        "option2": "Uber",
+        "option3": "Seamless",
+        "option4": "Google Maps",
         "isSent": false
-    }, {
+    },
+    {
+        "question_id": "HIM02",
+        "question_text": "Which of the Yuan's personal picture should the writer bring up in the discussion?",
+        "option1": "From a party",
+        "option2": "From a tourist location",
+        "option3": "From an event",
+        "option4": "At home",
+        "isSent": false
+    },
+    {
+        "question_id": "HIM03",
+        "question_text": "Which of the Carlie's personal picture should the writer bring up in the discussion?",
+        "option1": "Eating at McDonalds",
+        "option2": "At Prom 2017",
+        "option3": "Having tequila",
+        "option4": "at a Baseball game",
+        "isSent": false
+    },
+    {
         "question_id": "HER01",
-        "question_text": "Which one of the following books should Sam talk about to make a good impression with the Writer?",
-        "option1": "To Kill a Mockingbird",
-        "option2": "The Great Gatsby",
-        "option3": "Pride and Prejudice",
-        "option4": "Nineteen Eighty Four",
-        "isSent": false
-    }, {
-        "question_id": "HER02",
-        "question_text": "The writer has lied on his dating profile, which lie should Sam bring of to embarrass the Writer?",
+        "question_text": "The writer has lied on his dating profile, which lie should Carlie bring of to embarrass the Writer?",
         "option1": "Height",
         "option2": "Age",
-        "option3": "Description",
-        "option4": "Picture",
+        "option3": "Citizenship Status",
+        "option4": "Profession",
+        "isSent": false
+    },
+    {
+        "question_id": "HIM04",
+        "question_text": "What personal detail should the writer know about David?",
+        "option1": "a movie he hates",
+        "option2": "his favorite Christmas display place",
+        "option3": "a person he would read Harry Potter to",
+        "option4": "his worst boss",
+        "isSent": false
+    },
+    {
+        "question_id": "HIM05",
+        "question_text": "What should the writer say is common between him and David to make a good impression?",
+        "option1": "names of pet",
+        "option2": "neighbourhood",
+        "option3": "places they lived in before New York",
+        "option4": "relationship status",
         "isSent": false
     }
 ]
